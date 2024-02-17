161dcdd59f928bb1e826bb6d79a4169482aabf9c
franciellyfp <franciellyfpedroso@gmail.com>
2024-02-17 13:52:43 -0300

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://pt.khanacademy.org/',
-    //defaultCommandTimeout: 50000,
+    defaultCommandTimeout: 30000,
     setupNodeEvents() {
 
     },
